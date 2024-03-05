3406af8c2933b494185a34492ab70eb28664dae5
zablon <zablonoigo51@gmail.com>
2024-03-05 07:37:58 +0300

    setup test data

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Post
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class PostTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user=get_user_model().objects.create_user(
+            username="testuser",
+            password="secret",
+            email="testuser@mail.com"
+        )
+        cls.post=Post.objects.creat(
+            title="Demo title",
+            slug="demo-title",
+            author=cls.user,
+            content="Demo text"
+        )
+
