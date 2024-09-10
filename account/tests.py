07f0142c4138cb4b42b2b653bee96862e5592369
zablon <zablonoigo51@gmail.com>
2024-09-10 19:12:25 +0300

    Add test model manager

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class UsersMnagersTest(TestCase):
+    def test_create_user(self):
+        User=get_user_model()
+        user=User.objects.create_user(
+            email="test@user.com",
+            password="test"
+        )
