47dcdf0c08eb04a891f2b6dba28e7410e3a9d9c9
zablon <zablonoigo51@gmail.com>
2024-09-11 17:56:31 +0300

    Admin manager test

--- account/tests.py
+++ account/tests.py
@@ -21,3 +21,22 @@ class UsersMnagersTest(TestCase):
             User.objects.create_user(email="")
         with self.assertRaises(ValueError):
             User.objects.create_user(email="",password="test")
+
+    def test_create_superuser(self):
+        User=get_user_model()
+        admin_user=User.objects.create_superuser(email="superuser@mail.com", password="test")
+        self.assertEqual(admin_user.email, "superuser@mail.com")
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+
+        try:
+            self.assertIsNone(admin_user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(ValueError):
+            User.objects.create_user(
+                email="superuser@mail.com",
+                password="test",
+                is_superuser=False
+            )
