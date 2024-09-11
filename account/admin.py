27657cb0c613068b6ede6d89cd1e542ce9ab30e4
zablon <zablonoigo51@gmail.com>
2024-09-11 19:40:00 +0300

    removed users permissions

--- account/admin.py
+++ account/admin.py
@@ -7,23 +7,21 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ("email", "is_staff", "is_active",)
-    list_filter = ("email", "is_staff", "is_active",)
+    list_display = ("email", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active")
+
     fieldsets = (
         (None, {"fields": ("email", "password")}),
-        ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
+        ("Permissions", {"fields": ("is_staff", "is_active")}),
     )
     add_fieldsets = (
         (None, {
             "classes": ("wide",),
-            "fields": (
-                "email", "password1", "password2", "is_staff",
-                "is_active", "groups", "user_permissions"
-            )}
-        ),
+            "fields": ("email", "password1", "password2", "is_staff", "is_active"),
+        }),
     )
+    
     search_fields = ("email",)
     ordering = ("email",)
-
 
 admin.site.register(CustomUser, CustomUserAdmin)
