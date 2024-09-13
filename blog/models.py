04b961f3ef254185293ecf02b8693bbdbfdb758c
zablon <zablonoigo51@gmail.com>
2024-09-13 07:48:35 +0300

    create a model manager

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.utils import timezone
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 class Post(models.Model):
     title=models.CharField(max_length=200)
     slug=models.SlugField(max_length=200)
@@ -7,7 +11,8 @@ class Post(models.Model):
     publish=models.DateTimeField(default=timezone.now)
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now=True)
-
+    objects=models.Manager()
+    published=PublishedManager()
     class Meta:
         ordering=['-publish']
         indexes=[
