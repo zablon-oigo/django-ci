cc60c184e2a27cf33d9abba728caf4348fed1cd2
zablon <zablonoigo51@gmail.com>
2024-09-13 07:50:20 +0300

    create a list view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts=Post.published.all ()
+    return  render(request,'post/list.html',{'posts':posts})
