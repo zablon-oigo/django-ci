419f22a5e7fae5546744e4249cdc5fe3e9efda24
zablon <zablonoigo51@gmail.com>
2024-09-13 07:55:27 +0300

    add get object to retrieve desired post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import render,get_object_or_404
 from .models import Post
 
 def post_list(request):
@@ -7,8 +6,5 @@ def post_list(request):
     return  render(request,'post/list.html',{'posts':posts})
 
 def post_detail(request,id):
-    try:
-        post=Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise   Http404('No Post found.')
-    return render(request,'post/detail.html',{'post':post})
+   post=get_object_or_404(Post,id=id,status=Post.Status.PUBLISHED)
+   return render(request,'post/detail.html',{'post':post})
