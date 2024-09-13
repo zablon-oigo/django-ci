4094f8181607d6fe9588ad87df497b2dfbc61c24
zablon <zablonoigo51@gmail.com>
2024-09-13 07:52:40 +0300

    add detail view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.http import Http404
 from .models import Post
 
 def post_list(request):
     posts=Post.published.all ()
     return  render(request,'post/list.html',{'posts':posts})
+
+def post_detail(request,id):
+    try:
+        post=Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise   Http404('No Post found.')
+    return render(request,'post/detail.html',{'post':post})
