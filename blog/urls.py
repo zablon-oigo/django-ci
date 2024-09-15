b2efdf4036224eb1ae65588a3079fa261b3656ee
zablon <zablonoigo51@gmail.com>
2024-09-15 13:52:48 +0300

    Add app name

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import post_list,post_detail
-
+app_name='blog'
 urlpatterns=[
     path("",post_list,name="list"),
     path("<int:id>/",post_detail,name="detail"),
