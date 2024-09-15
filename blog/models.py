4c6f6d88df4d960b41ed41494faaac0ca90c5f82
zablon <zablonoigo51@gmail.com>
2024-09-15 13:48:58 +0300

    modifying the canonical url for posts

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-
+from django.urls import reverse
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(status=Post.Status.PUBLISHED)
@@ -21,3 +21,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,self.publish.month,self.publish.day,self.slug])
