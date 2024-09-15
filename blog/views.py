cc77f837b6a8aee0092d7c36f47479e990008ee4
zablon <zablonoigo51@gmail.com>
2024-09-15 13:46:19 +0300

    modifying the views

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,6 @@ def post_list(request):
     posts=Post.published.all ()
     return  render(request,'post/list.html',{'posts':posts})
 
-def post_detail(request,id):
-   post=get_object_or_404(Post,id=id,status=Post.Status.PUBLISHED)
+def post_detail(request,year,month,day,post):
+   post=get_object_or_404(Post,id=id,status=Post.Status.PUBLISHED,slug=post,publish__year=year,publish__month=month,publish__day=day)
    return render(request,'post/detail.html',{'post':post})
