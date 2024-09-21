e3d489205264e5a1f8171b70dc8c25888934c0d1
zablon <zablonoigo51@gmail.com>
2024-09-21 20:16:58 +0300

    Added permissions mixin

--- account/models.py
+++ account/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
 from django.utils import timezone
 from .managers import CustomUserManager
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractBaseUser,PermissionsMixin):
     email=models.EmailField(unique=True)
     is_staff=models.BooleanField(default=False)
     is_active=models.BooleanField(default=True)
