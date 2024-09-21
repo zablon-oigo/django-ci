986b7d66606c85e427eb2853dfd019f3acc9de62
zablon <zablonoigo51@gmail.com>
2024-09-21 20:16:08 +0300

    Added superuser field

--- account/models.py
+++ account/models.py
@@ -6,6 +6,7 @@ class CustomUser(AbstractBaseUser):
     email=models.EmailField(unique=True)
     is_staff=models.BooleanField(default=False)
     is_active=models.BooleanField(default=True)
+    is_superuser=models.BooleanField(default=False)
     date_joined=models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD="email"
