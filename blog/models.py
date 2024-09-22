fe56ee0c7f1fc2fc8a51242eb4c7e12cef8b1f6b
zablon <zablonoigo51@gmail.com>
2024-09-22 14:36:02 +0300

    Added author field

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
+from django.conf import settings
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 class Post(models.Model):
     title=models.CharField(max_length=200)
+    author=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     slug=models.SlugField(max_length=200,unique_for_date='publish')
     body=models.TextField()
     publish=models.DateTimeField(default=timezone.now)
