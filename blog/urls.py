fe9ce1541838c6d35a74d43481bf4f14c2a8538b
zablon <zablonoigo51@gmail.com>
2024-09-22 16:51:52 +0300

    Added url configuration

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from .views import post_list,post_detail
 app_name='blog'
 urlpatterns=[
     path("",post_list,name="list"),
-    path("<int:id>/",post_detail,name="detail"),
+    path("<int:year>/<int:month>/<int:day>/<slug:post>/",post_detail,name="detail"),
 ]
