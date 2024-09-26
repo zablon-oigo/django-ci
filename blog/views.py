e720df4cecd92bed4e13d24bd7b6e7eb5a5f0e50
zablon <zablonoigo51@gmail.com>
2024-09-26 13:42:49 +0300

    Added post share logic

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post
-
+from .forms import EmailPostForm
 def post_list(request):
     posts=Post.published.all ()
     return  render(request,'post/list.html',{'posts':posts})
@@ -8,3 +8,13 @@ def post_list(request):
 def post_detail(request,year,month,day,post):
    post=get_object_or_404(Post,status=Post.Status.PUBLISHED,slug=post,publish__year=year,publish__month=month,publish__day=day)
    return render(request,'post/detail.html',{'post':post})
+
+def post_share(request, post_id):
+    post=get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        form=EmailPostForm(request.POST)
+        if form.is_valid():
+            cd=form.cleaned_data
+    else:
+        form=EmailPostForm()
+    return render(request,'post/share.html',{'post':post,'form':form})
