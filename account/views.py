f404b674c110bbcb787654e711b39f148df21cac
zablon <zablonoigo51@gmail.com>
2024-10-07 17:16:19 +0300

    added user login

--- account/views.py
+++ account/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import CustomUser
+def sign_in(request):
+    if request.method == 'POST':
+        form=LoginForm(request.POST)
+        if form.is_valid():
+            email=form.cleaned_data['email']
+            password=form.cleaned_data['password']
+            
+            user=authenticate(request, email=email, password=password)
+            
+            if user is not None and user.is_active:
+                login(request, user)
+                messages.success(request, 'Login request was successfull')
+                return redirect('list')
+        messages.error(request, 'Invalid username or password')
+    form=LoginForm()
+    return render(request,'accounts/login.html',{'form':form})
