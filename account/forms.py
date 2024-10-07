e2d48878ddcfd9a50af9ee0e931f2e456ab0e974
zablon <zablonoigo51@gmail.com>
2024-10-07 17:09:11 +0300

    user registration form

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 from .models import CustomUser
-
+from django import forms
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model=CustomUser
@@ -10,3 +10,14 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model=CustomUser
         fields=("email",)
+
+class RegisterUser(forms.ModelForm):
+    class Meta:
+        model=CustomUser
+        fields=['email','password1','password2']
+
+    def clean_password(self):
+        cd=self.cleaned_data
+        if cd['password1'] != cd['password2']:
+            raise forms.ValidationError('Password did not match')
+        return cd['password2']
