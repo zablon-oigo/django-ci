26c4261402bdfe56ad36efd6146ea77ee4e03732
zablon <zablonoigo51@gmail.com>
2024-10-12 14:53:11 +0300

    additional url

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ from .views import post_detail, post_list, post_share
 
 app_name = "blog"
 urlpatterns = [
-    path("<tag:tag_slug>/", post_list, name="list"),
+    path("", post_list, name="list"),
+    path("<slug:tag_slug>/", post_list, name="post_list_by_tag"),
     path("<int:year>/<int:month>/<int:day>/<slug:post>/", post_detail, name="detail"),
     path("<int:post_id>/share/", post_share, name="post_share"),
 ]
