5f89c3b10a22c40bae8125ecefc13d6cd010106b
zablon <zablonoigo51@gmail.com>
2024-10-13 15:02:16 +0300

    add username field

--- account/managers.py
+++ account/managers.py
@@ -2,12 +2,14 @@ from django.contrib.auth.base_user import BaseUserManager
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, email, username, password, **extra_fields):
         if not email:
             raise ValueError("The Email must be set")
+        if not username:
+            raise ValueError("The Username must be set")
 
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=email, username=username, **extra_fields)
         user.set_password(password)
         user.save()
         return user
