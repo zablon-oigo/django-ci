cef401e6e75102cfa3f9ee8c748af6e082882d7c
zablon <zablonoigo51@gmail.com>
2024-10-13 15:00:36 +0300

    update users sign up logic

--- account/views.py
+++ account/views.py
@@ -1,8 +1,14 @@
+from datetime import timedelta
+
+import pyotp
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect, render
+from django.utils import timezone
 
-from .forms import LoginForm, RegisterUser
+from .forms import LoginForm, RegisterUser, VerifyForm
+from .models import CustomUser
+from .utils import send_code_to_user
 
 
 def sign_in(request):
@@ -30,10 +36,13 @@ def sign_up(request):
             user = form.save(commit=False)
             user.email = user.email.lower()
             user.save()
-            return render(request, "accounts/register.html", {"user": user})
+
+            send_code_to_user(request, user.email)
+            return redirect("verify")
 
     else:
         form = RegisterUser()
+
     return render(request, "accounts/register.html", {"form": form})
 
 
@@ -41,3 +50,35 @@ def sign_out(request):
     logout(request)
     messages.success(request, "Logout request was successfull")
     return redirect("login")
+
+
+def verify_otp_view(request):
+    if request.method == "POST":
+        form = VerifyForm(request.POST)
+        if form.is_valid():
+            user_email = request.session.get("user_email")
+            user = CustomUser.objects.get(email=user_email)
+
+            otp_expiry_time = user.otp_created_at + timedelta(minutes=300)
+            if timezone.now() > otp_expiry_time:
+                messages.error(request, "OTP has expired. Please request a new one.")
+                return redirect("resend_otp")
+            otp_instance = pyotp.TOTP(user.secret_key, interval=300)
+            user_otp = form.cleaned_data["otp"]
+            if otp_instance.verify(user_otp, valid_window=1):
+                messages.success(request, "OTP verified successfully!")
+                return redirect("blog:list")
+            else:
+                messages.error(request, "Invalid OTP. Please try again.")
+    else:
+        form = VerifyForm()
+
+    return render(request, "accounts/verify.html", {"form": form})
+
+
+def resend_otp_view(request):
+    user_email = request.session.get("user_email")
+    user = CustomUser.objects.get(email=user_email)
+    send_code_to_user(request, user.email)
+    messages.success(request, "A new OTP has been sent to your email.")
+    return redirect("verify")
