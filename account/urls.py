2ef2e1702e4e575d179ba360fb1becd9785a9af4
zablon <zablonoigo51@gmail.com>
2024-10-14 06:23:55 +0300

    add url path

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import sign_in, sign_out, sign_up
+from .views import resend_otp_view, sign_in, sign_out, sign_up, verify_otp_view
 
 urlpatterns = [
     path("register/", sign_up, name="register"),
     path("login/", sign_in, name="login"),
+    path("verify/", verify_otp_view, name="verify"),
     path("logout/", sign_out, name="logout"),
+    path("resend-otp/", resend_otp_view, name="resend_otp"),
 ]
