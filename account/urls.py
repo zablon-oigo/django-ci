31991de0557f31fc08918ece6fc0e8a60ea8d3f3
zablon <zablonoigo51@gmail.com>
2024-10-15 20:47:41 +0300

    Add password rest url path

--- account/urls.py
+++ account/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from .views import resend_otp_view, sign_in, sign_out, sign_up, verify_otp_view
@@ -8,4 +9,12 @@ urlpatterns = [
     path("verify/", verify_otp_view, name="verify"),
     path("logout/", sign_out, name="logout"),
     path("resend-otp/", resend_otp_view, name="resend_otp"),
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="account/password-reset.html"
+        ),
+        name="password-reset",
+    ),
+    path(),
 ]
