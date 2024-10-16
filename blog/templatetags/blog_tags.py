c91332c12a18f0056a330c722efe1e9043571c93
zablon <zablonoigo51@gmail.com>
2024-10-16 21:57:42 +0300

    Add inclusion template tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -8,3 +8,9 @@ register = template.Library()
 @register.simple_tag
 def total_posts():
     return Post.published.count()
+
+
+@register.inclusion_tag("blog/latest.html")
+def show_latest_post(count=5):
+    latest_posts = Post.published.order_by("-publish")[:count]
+    return {"latest_posts": latest_posts}
