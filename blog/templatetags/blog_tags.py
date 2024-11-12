8133cea6907ec0a0bf7f336ed6f4775cbeba5dd5
zablon <zablonoigo51@gmail.com>
2024-11-12 22:27:12 +0300

    Add markdown template syntax

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -14,3 +14,8 @@ def total_posts():
 def show_latest_post(count=5):
     latest_posts = Post.published.order_by("-publish")[:count]
     return {"latest_posts": latest_posts}
+
+
+# @register.filter(name="markdown")
+# def markdown_format(text):
+#     return mark_safe(markdown.markdown(text))
