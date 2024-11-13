abb98ce56e0cea2b95b642f9ef485f092b14eb9f
zablon <zablonoigo51@gmail.com>
2024-11-13 08:00:02 +0300

    Add custom feeds for post

---
+++ blog/feeds.py
@@ -0,0 +1,13 @@
+from django.contrib.syndication.views import Feed
+from django.urls import reverse_lazy
+
+from .models import Post
+
+
+class LatestPostsFeed(Feed):
+    title = "test post"
+    link = reverse_lazy("blog:post_list")
+    description = "New post test"
+
+    def items(self):
+        return Post.published.all()[:5]
