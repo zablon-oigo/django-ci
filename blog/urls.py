f81005abd81be587e57928d7e50ee1386514ec9e
zablon <zablonoigo51@gmail.com>
2024-11-16 18:08:39 +0300

    Add search path config

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
 from .feeds import LatestPostsFeed
-from .views import post_detail, post_list, post_share
+from .views import post_detail, post_list, post_search, post_share
 
 app_name = "blog"
 urlpatterns = [
     path("", post_list, name="list"),
-    path("<slug:tag_slug>/", post_list, name="post_list_by_tag"),
     path("<int:year>/<int:month>/<int:day>/<slug:post>/", post_detail, name="detail"),
     path("<int:post_id>/share/", post_share, name="post_share"),
     path("feed/", LatestPostsFeed(), name="post-feed"),
+    path("search/", post_search, name="post_search"),
+    path("<slug:tag_slug>/", post_list, name="post_list_by_tag"),
 ]
